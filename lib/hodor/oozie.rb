f0895409e9e29cdb9a4cad1888288e27845aca4e
Dean Hallman <dhallman@scrippsnetworks.com>
2016-08-25 21:29:02 -0400

    Initial commit

---
+++ lib/hodor/oozie.rb
@@ -0,0 +1,7 @@
+require "hodor/oozie/version"
+
+module Hodor
+  module Oozie
+    # Your code goes here...
+  end
+end
---
+++ lib/hodor/oozie/version.rb
@@ -0,0 +1,5 @@
+module Hodor
+  module Oozie
+    VERSION = "0.1.0"
+  end
+end
